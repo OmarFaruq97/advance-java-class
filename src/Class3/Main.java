0ee0f771599557f2115aced0677a2c316608ccbc
OmarFaruq97 <omarfaruq9723@gmail.com>
2025-01-20 09:51:34 +0600

    here inserting select initial

---
+++ src/Class3/Main.java
@@ -0,0 +1,12 @@
+package Class3;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        SelectStudentPostgreSQL selectStudentPostgreSQL = new SelectStudentPostgreSQL();
+
+        selectStudentPostgreSQL.selectAllStudents();
+    }
+}
+
